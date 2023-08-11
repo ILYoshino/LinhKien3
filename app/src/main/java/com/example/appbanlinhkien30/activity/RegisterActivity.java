2a10dcca58f08fa84a58a3ea4fed87a96bd455a7
Thuan <giathuanvo2708@gmail.com>
2023-08-11 16:35:34 +0700

    modify RegisterActivity

--- app/src/main/java/com/example/appbanlinhkien30/activity/RegisterActivity.java
+++ app/src/main/java/com/example/appbanlinhkien30/activity/RegisterActivity.java
@@ -3,14 +3,22 @@ package com.example.appbanlinhkien30.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.appbanlinhkien30.R;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    EditText edtUsername, edtPassword, edtPhone;
+    Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        edtUsername = (EditText) findViewById(R.id.edtUsernameRegister);
+        edtPassword = (EditText) findViewById(R.id.edtPasswordRegister);
+        edtPhone = (EditText) findViewById(R.id.edtPhone);
+        btnRegister = (Button) findViewById(R.id.btnCreateAccount);
     }
 }
