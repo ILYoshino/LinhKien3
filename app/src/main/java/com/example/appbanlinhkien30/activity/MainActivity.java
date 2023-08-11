44bfebc70b6871590df45da24f23c1fde1147dae
ILYoshino <2051012022dang@ou.edu.vn>
2023-08-11 12:04:38 +0700

    new one

---
+++ app/src/main/java/com/example/appbanlinhkien30/activity/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.appbanlinhkien30.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ViewFlipper;
+
+import com.bumptech.glide.Glide;
+import com.example.appbanlinhkien30.R;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerViewMHC;
+    NavigationView navigationView;
+    ListView listViewMHC;
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Reflect();
+        ActionBar();
+        ActionViewFlipper();
+    }
+
+    private void ActionBar() {
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    private void ActionViewFlipper() {
+        List<String> list = new ArrayList<>();
+        list.add("https://cdn.discordapp.com/attachments/1131867639665406012/1131872096235049060/image.png");
+        list.add("https://cdn.discordapp.com/attachments/1131867639665406012/1131872129814638642/image.png");
+        list.add("https://cdn.discordapp.com/attachments/1131867639665406012/1131867768707354624/image.png");
+        for (int i = 0; i<list.size(); i++){
+            ImageView imageView = new ImageView(getApplicationContext());
+            Glide.with(getApplicationContext()).load(list.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+
+        }
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.setAutoStart(true);
+        Animation slide_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_right);
+        Animation slide_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out_right);
+        viewFlipper.setInAnimation(slide_in);
+        viewFlipper.setOutAnimation(slide_out);
+    }
+
+    private void Reflect() {
+        toolbar = findViewById(R.id.toolbarmhc);
+        viewFlipper = findViewById(R.id.viewflipper);
+        recyclerViewMHC = findViewById(R.id.recyclerview);
+        listViewMHC = findViewById(R.id.listviewmhc);
+        navigationView = findViewById(R.id.navigation);
+        drawerLayout = findViewById(R.id.drawerlayout);
+//        //List
+//        listCategory = new ArrayList<>();
+//        //Adapter
+//        categoryAdapter = new CatergoryAdapter(getApplicationContext(), listCategory);
+//        listViewMHC.setAdapter(categoryAdapter);
+    }
+}
