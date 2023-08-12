18873e448c05407d11849747e33daea0d7050596
Thuan <giathuanvo2708@gmail.com>
2023-08-12 23:47:57 +0700

    add LoadingActivity

--- app/src/main/java/com/example/appbanlinhkien30/activity/HomeActivity.java
+++ app/src/main/java/com/example/appbanlinhkien30/activity/HomeActivity.java
@@ -10,14 +10,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.appbanlinhkien30.R;
-import com.example.appbanlinhkien30.model.User;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
 
 public class HomeActivity extends AppCompatActivity {
     private Button btnLogin, btnRegister;
@@ -45,34 +38,5 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-    }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (currentUser != null) {
-            DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("User")
-                                                        .child(currentUser.getUid());
-            userRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    if (snapshot.exists()) {
-                        User user = snapshot.getValue(User.class);
-                        if (user.getIsAdmin() == 0) {
-                            startActivity(new Intent(HomeActivity.this, MainActivity.class));
-                        }
-                        else {
-                            startActivity(new Intent(HomeActivity.this, AdminActivity.class));
-                        }
-                        finish();
-                    }
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-
-                }
-            });
-        }
     }
 }
